424a5316294a98201d014aaa367e5ddcc2962fef
isabel-yue <isabelyue.design@gmail.com>
2023-11-14 13:05:12 -0700

    js

--- scripts/links.js
+++ scripts/links.js
@@ -4,53 +4,60 @@ const baseURL = "https://isabel-yue.github.io/wdd230/";
 const linksURL = "https://isabel-yue.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
-  }
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data);
+}
 
-  getLinks();
+function displayLinks(weeks) {
+  const learningActivitySection = document.getElementById("learning-activity");
 
-  async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    displayLinks(data);
-  }
+  // Clear existing content
+  learningActivitySection.innerHTML = "";
 
-  function displayLinks(weeks) {
-    const learningActivitySection = document.getElementById("learning-activity");
-  
-    // Clear existing content
-    learningActivitySection.innerHTML = "";
-  
-    // Loop through each week
-    weeks.forEach((week) => {
-      const weekElement = document.createElement("div");
-      weekElement.classList.add("week");
-  
-      // Create week title
-      const weekTitle = document.createElement("h3");
-      weekTitle.textContent = week.week;
-      weekElement.appendChild(weekTitle);
-  
-      // Create links list
-      const linksList = document.createElement("ul");
-  
-      // Loop through each link in the week
-      week.links.forEach((link) => {
-        const listItem = document.createElement("li");
-        const anchor = document.createElement("a");
-        anchor.href = link.url.startsWith("http") ? link.url : baseURL + link.url;
-        anchor.textContent = link.title;
-        listItem.appendChild(anchor);
-        linksList.appendChild(listItem);
-      });
-  
-      // Append links list to the week element
-      weekElement.appendChild(linksList);
-  
-      // Append week element to the learning activity section
-      learningActivitySection.appendChild(weekElement);
+  // Loop through each week
+  weeks.forEach((week) => {
+    const weekElement = document.createElement("div");
+    weekElement.classList.add("week");
+
+    // Create a single list item for the week with its links
+    const listItem = document.createElement("li");
+
+    // Create a span to hold the week title
+    const weekTitle = document.createElement("span");
+    weekTitle.textContent = week.week;
+
+    // Append the week title to the list item
+    listItem.appendChild(weekTitle);
+
+    // Create a span to hold all the links for the week
+    const linksSpan = document.createElement("span");
+
+    // Loop through each link in the week
+    week.links.forEach((link, index) => {
+      const anchor = document.createElement("a");
+      anchor.href = link.url.startsWith("http") ? link.url : baseURL + link.url;
+      anchor.textContent = link.title;
+
+      // Append the anchor to the links span
+      linksSpan.appendChild(anchor);
+
+      // If it's not the last link, add a comma and space
+      if (index < week.links.length - 1) {
+        linksSpan.appendChild(document.createTextNode(", "));
+      }
     });
-  }
-  
+
+    // Append the links span to the list item
+    listItem.appendChild(linksSpan);
+
+    // Append the list item to the week element
+    weekElement.appendChild(listItem);
+
+    // Append week element to the learning activity section
+    learningActivitySection.appendChild(weekElement);
+  });
+}
+
+// Call the getLinks function to fetch and display the links
+getLinks();
