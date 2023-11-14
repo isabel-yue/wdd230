2b2d2d46da5df1e51de7e2e93a1f66cd970ebae3
isabel-yue <isabelyue.design@gmail.com>
2023-11-14 13:32:13 -0700

    weather

--- scripts/weather.js
+++ scripts/weather.js
@@ -7,44 +7,37 @@ const captionDesc = document.querySelector(`figcaption`);
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.81&lon=-111.48&units=imperial&appid=db0af141a6c1426e9777be9c2d558fb6';
 
 // Define asynchronous function for API fetch
-async function apiFetch() {
-    try {
-        // Fetch data from the URL
-        const response = await fetch(url);
-
-        // Check if the response is OK
-        if (response.ok) {
-            // Convert response to JSON
-            const data = await response.json();
-
-            // Output data to the console for testing
-            console.log(data);
-
-            // Assuming you have a function to update the HTML elements
-            displayResults(data);
-
-        } else {
-            // Throw an error with the response text
-            throw new Error(await response.text());
-        }
-    } catch (error) {
-        // Output any errors to the console
-        console.error(error);
+fetch(url)
+  .then(response => {
+    // Check if the response is OK
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
-}
+
+    // Convert response to JSON
+    return response.json();
+  })
+  .then(data => {
+    // Output data to the console for testing
+    console.log(data);
+
+    // Assuming you have a function to update the HTML elements
+    displayResults(data);
+  })
+  .catch(error => {
+    // Output any errors to the console
+    console.error(error);
+  });
 
 // Assuming you have a function to update the HTML elements
 function displayResults(data) {
-    // Update HTML elements with the retrieved data
-    currentTemp.textContent = `${data.main.temp}°F`;
-    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    captionDesc.textContent = data.weather[0].description;
+  // Update HTML elements with the retrieved data
+  currentTemp.textContent = `${data.main.temp}°F`;
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  captionDesc.textContent = data.weather[0].description;
 
-    // Output to console for testing
-    console.log(`Temperature: ${data.main.temp}°F`);
-    console.log(`Weather Description: ${data.weather[0].description}`);
-    console.log(`Icon URL: https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+  // Output to console for testing
+  console.log(`Temperature: ${data.main.temp}°F`);
+  console.log(`Weather Description: ${data.weather[0].description}`);
+  console.log(`Icon URL: https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
 }
-
-// Invoke the apiFetch function
-apiFetch();
