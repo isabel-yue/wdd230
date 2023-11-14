107fa014cf2c300a35a9bd5bccfb6ca901c2e8ca
isabel-yue <isabelyue.design@gmail.com>
2023-11-14 13:26:42 -0700

    weather

--- scripts/weather.js
+++ scripts/weather.js
@@ -3,48 +3,34 @@ const currentTemp = document.querySelector(`#current-temp`);
 const weatherIcon = document.querySelector(`#weather-icon`);
 const captionDesc = document.querySelector(`figcaption`);
 
+// Declare and assign URL
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.81&lon=-111.48&units=imperial&appid=db0af141a6c1426e9777be9c2d558fb6';
 
- // Declare and assign URL
- const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.81&lon=-111.48&units=imperial&appid=db0af141a6c1426e9777be9c2d558fb6';
-
- // Define asynchronous function for API fetch
+// Define asynchronous function for API fetch
 async function apiFetch() {
     try {
-      // Fetch data from the URL
-      const response = await fetch(url);
-  
-      // Check if the response is OK
-      if (response.ok) {
-        // Convert response to JSON
-        const data = await response.json();
-  
-        // Output data to the console for testing
-        console.log(data);
-  
-        // Call the displayResults function here (to be implemented)
-        displayResults(data);
-      } else {
-        // Throw an error with the response text
-        throw new Error(await response.text());
-      }
+        // Fetch data from the URL
+        const response = await fetch(url);
+
+        // Check if the response is OK
+        if (response.ok) {
+            // Convert response to JSON
+            const data = await response.json();
+
+            // Output data to the console for testing
+            console.log(data);
+
+            // Call the displayResults function here (to be implemented)
+            displayResults(data);
+        } else {
+            // Throw an error with the response text
+            throw new Error(await response.text());
+        }
     } catch (error) {
-      // Output any errors to the console
-      console.error(error);
+        // Output any errors to the console
+        console.error(error);
     }
-  }
-// Invoke the apiFetch function
-apiFetch();
-
-// Assuming the data structure from the API response
-const currentTemperature = data.main.temp;
-const weatherDescription = data.weather[0].description;
-const iconCode = data.weather[0].icon;
-const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
-
-// Output to console for testing
-console.log(`Temperature: ${currentTemperature}°F`);
-console.log(`Weather Description: ${weatherDescription}`);
-console.log(`Icon URL: ${iconUrl}`);
+}
 
 // Assuming you have a function to update the HTML elements
 function displayResults(data) {
@@ -52,4 +38,12 @@ function displayResults(data) {
     currentTemp.textContent = `${data.main.temp}°F`;
     weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     captionDesc.textContent = data.weather[0].description;
-  }
+
+    // Output to console for testing
+    console.log(`Temperature: ${data.main.temp}°F`);
+    console.log(`Weather Description: ${data.weather[0].description}`);
+    console.log(`Icon URL: https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+}
+
+// Invoke the apiFetch function
+apiFetch();
