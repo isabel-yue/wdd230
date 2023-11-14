0087e3d80991d7aab9f329321d140274bcc1ab8c
isabel-yue <isabelyue.design@gmail.com>
2023-11-14 13:18:31 -0700

    push

--- scripts/links.js
+++ scripts/links.js
@@ -3,16 +3,15 @@
 const baseURL = "https://isabel-yue.github.io/wdd230/";
 const linksURL = "https://isabel-yue.github.io/wdd230/data/links.json";
 
-
 async function getLinks() {
-    try {
-      const response = await fetch(linksURL);
-      const data = await response.json();
-      displayLinks(data);
-    } catch (error) {
-      console.error("Error fetching or parsing JSON:", error);
-    }
+  try {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  } catch (error) {
+    console.error("Error fetching or parsing JSON:", error);
   }
+}
 
 function displayLinks(weeks) {
   const learningActivitySection = document.getElementById("learning-activity");
@@ -20,42 +19,50 @@ function displayLinks(weeks) {
   // Clear existing content
   learningActivitySection.innerHTML = "";
 
-  // Loop through each week
-  weeks.forEach((week) => {
-    const weekElement = document.createElement("div");
-    weekElement.classList.add("week");
+  // Ensure that 'weeks' is an array with the expected structure
+  if (Array.isArray(weeks) && weeks.length > 0 && weeks[0].hasOwnProperty("week") && weeks[0].hasOwnProperty("links")) {
+    // Loop through each week
+    weeks.forEach((week) => {
+      const weekElement = document.createElement("div");
+      weekElement.classList.add("week");
 
-    // Create a single list item for the week with its links
-    const listItem = document.createElement("li");
+      // Create a single list item for the week with its links
+      const listItem = document.createElement("li");
 
-    // Create a span to hold the week title
-    const weekTitle = document.createElement("span");
-    weekTitle.textContent = `${week.week}: `;
+      // Create a span to hold the week title
+      const weekTitle = document.createElement("span");
+      weekTitle.textContent = `${week.week}: `;
 
-    // Append the week title to the list item
-    listItem.appendChild(weekTitle);
+      // Append the week title to the list item
+      listItem.appendChild(weekTitle);
 
-    // Loop through each link in the week
-    week.links.forEach((link, index) => {
-      const anchor = document.createElement("a");
-      anchor.href = link.url.startsWith("http") ? link.url : baseURL + link.url;
-      anchor.textContent = link.title;
+      // Create a span to hold all the links for the week
+      const linksSpan = document.createElement("span");
 
-      // Append the anchor to the list item
-      listItem.appendChild(anchor);
+      // Loop through each link in the week
+      week.links.forEach((link, index) => {
+        const anchor = document.createElement("a");
+        anchor.href = link.url.startsWith("http") ? link.url : baseURL + link.url;
+        anchor.textContent = link.title;
 
-      // If it's not the last link, add a pipe separator
-      if (index < week.links.length - 1) {
-        listItem.appendChild(document.createTextNode(" | "));
-      }
+        // Append the anchor to the list item
+        listItem.appendChild(anchor);
+
+        // If it's not the last link, add a pipe separator
+        if (index < week.links.length - 1) {
+          listItem.appendChild(document.createTextNode(" | "));
+        }
+      });
+
+      // Append the list item to the week element
+      weekElement.appendChild(listItem);
+
+      // Append week element to the learning activity section
+      learningActivitySection.appendChild(weekElement);
     });
-
-    // Append the list item to the week element
-    weekElement.appendChild(listItem);
-
-    // Append week element to the learning activity section
-    learningActivitySection.appendChild(weekElement);
-  });
+  } else {
+    console.error("Invalid data format. Expected an array of weeks with the correct structure.");
+  }
 }
 
 // Call the getLinks function to fetch and display the links
