e0c70ca57a5b3d9c726d9ab23b033508f024f8fe
isabel-yue <isabelyue.design@gmail.com>
2023-11-14 12:48:57 -0700

    new

---
+++ scripts/links.js
@@ -0,0 +1,54 @@
+// links.js
+
+// Step 2: Create a baseURL variable
+const baseURL = "https://isabel-yue.github.io/wdd230/";
+
+// Step 3: Add a variable named linksURL
+const linksURL = "https://isabel-yue.github.io/wdd230//data/links.json";
+
+
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data);
+}
+
+function displayLinks(weeks) {
+  const learningActivitySection = document.getElementById("learning-activity");
+
+  // Clear existing content
+  learningActivitySection.innerHTML = "";
+
+  // Loop through each week
+  weeks.forEach((week) => {
+    const weekElement = document.createElement("div");
+    weekElement.classList.add("week");
+
+    // Create week title
+    const weekTitle = document.createElement("h3");
+    weekTitle.textContent = week.week;
+    weekElement.appendChild(weekTitle);
+
+    // Create links list
+    const linksList = document.createElement("ul");
+
+    // Loop through each link in the week
+    week.links.forEach((link) => {
+      const listItem = document.createElement("li");
+      const anchor = document.createElement("a");
+      anchor.href = link.url.startsWith("http") ? link.url : baseURL + link.url;
+      anchor.textContent = link.title;
+      listItem.appendChild(anchor);
+      linksList.appendChild(listItem);
+    });
+
+    // Append links list to the week element
+    weekElement.appendChild(linksList);
+
+    // Append week element to the learning activity section
+    learningActivitySection.appendChild(weekElement);
+  });
+}
+
+// Call the getLinks function to fetch and display the links
+getLinks();
---
+++ scripts/weather.js
@@ -0,0 +1,55 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector(`#current-temp`);
+const weatherIcon = document.querySelector(`#weather-icon`);
+const captionDesc = document.querySelector(`figcaption`);
+
+
+ // Declare and assign URL
+ const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.81&lon=-111.48&units=imperial&appid=db0af141a6c1426e9777be9c2d558fb6';
+
+ // Define asynchronous function for API fetch
+async function apiFetch() {
+    try {
+      // Fetch data from the URL
+      const response = await fetch(url);
+  
+      // Check if the response is OK
+      if (response.ok) {
+        // Convert response to JSON
+        const data = await response.json();
+  
+        // Output data to the console for testing
+        console.log(data);
+  
+        // Call the displayResults function here (to be implemented)
+        displayResults(data);
+      } else {
+        // Throw an error with the response text
+        throw new Error(await response.text());
+      }
+    } catch (error) {
+      // Output any errors to the console
+      console.error(error);
+    }
+  }
+// Invoke the apiFetch function
+apiFetch();
+
+// Assuming the data structure from the API response
+const currentTemperature = data.main.temp;
+const weatherDescription = data.weather[0].description;
+const iconCode = data.weather[0].icon;
+const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+
+// Output to console for testing
+console.log(`Temperature: ${currentTemperature}°F`);
+console.log(`Weather Description: ${weatherDescription}`);
+console.log(`Icon URL: ${iconUrl}`);
+
+// Assuming you have a function to update the HTML elements
+function displayResults(data) {
+    // Update HTML elements with the retrieved data
+    currentTemp.textContent = `${data.main.temp}°F`;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    captionDesc.textContent = data.weather[0].description;
+  }
